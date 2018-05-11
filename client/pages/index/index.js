1ca0bdb0e07c6b0399d1c79cc218440483b5f1d4
szhclaye <328044021@qq.com>
2018-05-11 23:18:56 +0800

    index

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -8,6 +8,16 @@ Page({
         userInfo: {},
         logged: false,
         takeSession: false,
+        /**轮播设置 */
+        imgUrls: [
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        indicatorDots: true,
+        autoplay: true,
+        interval: 5000,
+        duration: 1000
     },
 
     // 用户登录示例
